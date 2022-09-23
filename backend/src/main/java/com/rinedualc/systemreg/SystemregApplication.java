3b25ea9aa6634c9f8447516e95dd04f494eee0a9
ClaudenirFer <claudenir_fer@yahoo.com.br>
2022-09-23 12:27:27 -0300

    feat(backend) :  adiciona o backend ao projeto

---
+++ backend/src/main/java/com/rinedualc/systemreg/SystemregApplication.java
@@ -0,0 +1,13 @@
+package com.rinedualc.systemreg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemregApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemregApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rinedualc/systemreg/SystemregApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rinedualc.systemreg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemregApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
