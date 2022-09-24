c1a9ffa90db1cf4cf8503b60a85e66968433a5db
ClaudenirFer <claudenir_fer@yahoo.com.br>
2022-09-24 12:12:23 -0300

    feat(backend/API/sale) :  adiciona o SaleRepository

---
+++ backend/src/main/java/com/rinedualc/systemreg/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.rinedualc.systemreg.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rinedualc.systemreg.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
