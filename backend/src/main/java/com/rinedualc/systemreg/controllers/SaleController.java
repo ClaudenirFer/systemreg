fdcddbcc29db70ba9f2cec22441242565a8b96fb
ClaudenirFer <claudenir_fer@yahoo.com.br>
2022-09-24 12:11:11 -0300

    feat(backend/API/sale) :  adiciona o SaleController

---
+++ backend/src/main/java/com/rinedualc/systemreg/controllers/SaleController.java
@@ -0,0 +1,24 @@
+package com.rinedualc.systemreg.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rinedualc.systemreg.entities.Sale;
+import com.rinedualc.systemreg.services.SaleService;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+	
+	@Autowired
+	private SaleService service;
+	
+	@GetMapping
+	public List<Sale> findSales() {
+		return service.findSales();
+	}
+}
