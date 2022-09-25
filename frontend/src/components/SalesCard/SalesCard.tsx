21d7375fb65bd41ba2efdbc318acdb4d992c43a5
ClaudenirFer <claudenir_fer@yahoo.com.br>
2022-09-25 00:59:36 -0300

    feat(frontend/SalesCard component) :  adiciona a requisição dos sales com axios no useEffect

--- frontend/src/components/SalesCard/SalesCard.tsx
+++ frontend/src/components/SalesCard/SalesCard.tsx
@@ -1,15 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NotificationButton from "../NotificationButton/NotificationButton";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "./salesCard.css";
+import axios from "axios";
 
 function SalesCard() {
+
   const min = new Date(new Date().setDate(new Date().getDate() - 365));
   const max = new Date();
 
   const [minDate, setMinDate] = useState(min);
   const [maxDate, setMaxDate] = useState(max);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/sales")
+      .then(response => {
+        console.log(response.data)
+      })
+  }, []);
 
   return (
     <div className="systemreg-card">
